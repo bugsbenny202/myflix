d6290beead9ec9f5b96107f12125375678d1cc85
bugsbenny202 <138828081+bugsbenny202@users.noreply.github.com>
2025-10-10 08:47:11 -0700

    update

---
+++ public/config.js
@@ -0,0 +1,18 @@
+// --- App Configuration ---
+// Paste your actual keys and configuration object below.
+// This file will be loaded directly by the browser.
+
+window.APP_CONFIG = {
+  TMDB_API_KEY: "1d573b23a7d971a0885d4ab0c1efdde2",
+  FIREBASE_CONFIG: {
+    // PASTE YOUR ENTIRE FIREBASE CONFIG OBJECT HERE
+    // For example:
+    apiKey: "AIzaSyA-gJRyf1L-PmPlyDzIq5_Gz1bFdnOZ4YI",
+  authDomain: "myflix-fd0c8.firebaseapp.com",
+  projectId: "myflix-fd0c8",
+  storageBucket: "myflix-fd0c8.firebasestorage.app",
+  messagingSenderId: "448506271719",
+  appId: "1:448506271719:web:7663dcbecf2cf869490f42",
+  measurementId: "G-C53DEZW2JF"
+  }
+};
